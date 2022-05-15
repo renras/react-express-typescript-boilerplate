757f35ce2501f4d8a843005f52bbc78d3ccbc2b8
Renzo Visperas <renzovisperas07@gmail.com>
2022-05-15 18:08:58 +0800

    fix errors

--- server.ts
+++ server.ts
@@ -1,24 +1,21 @@
 import express from "express";
-import webpack from "webpack";
-import { Configuration } from "webpack";
+import webpack, { Configuration } from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
 import webpackHotMiddleware from "webpack-hot-middleware";
+import config from "./webpack.config";
 
 const app = express();
-import config from "./webpack.config";
 const compiler = webpack(config as Configuration);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
+	webpackDevMiddleware(compiler, {
+		publicPath: config.output.publicPath,
+	})
 );
 
 app.use(webpackHotMiddleware(compiler));
 
 // Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!\n");
-});
+app.listen(3000, () => console.log("Example app listening on port 3000!\n"));
