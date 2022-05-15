d4a89895a000c76e2cc2584f765b8ce95dc5bd87
renras <renzovisperas07@gmail.com>
2022-05-15 15:55:57 +0800

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,52 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const config = {
-  mode: "development",
-  entry: [
-    "webpack-hot-middleware/client?reload=true",
-    "./src/index.ts", // Your entry point
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+module.exports = {
+	mode: "development",
+	entry: [
+		"webpack-hot-middleware/client?reload=true",
+		"./src/index.tsx", // Your entry point
+	],
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+		hot: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+		publicPath: "/",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 };
-
-export default config;
