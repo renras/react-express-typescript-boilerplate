97deb48027b7e1b2fba3d3b1ddb70667609a9ee2
Renzo Visperas <renzovisperas07@gmail.com>
2022-05-16 21:37:25 +0800

    add babel for react

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,12 @@ module.exports = {
 			},
 			{
 				test: /\.tsx?$/,
-				use: "ts-loader",
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
 				exclude: /node_modules/,
 			},
 			{
